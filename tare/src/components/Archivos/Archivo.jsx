deff37cdcf0934fb0d698dfd3150572bd568f2a6
A01722540 <a01722540@tec.mx>
2024-05-25 12:17:11 -0600

    Mi primer commit

---
+++ tare/src/components/Archivos/Archivo.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Archivo.css'
+
+export default function({props}){
+    return(
+        <>
+        <div class = 'examen-component'>
+            <div class= 'block icon-innershadow'>
+                <img src="./src/assets/examen-icon.svg" alt="Examen"/>
+            </div>
+
+            <p>Nombre</p>
+        </div>
+
+        </>
+    )
+}
--- tare/src/pages/Home/Home.jsx
+++ tare/src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Archivo from '../../components/Archivos/Archivo';
 
 export default function Home() {
     return (
         <div>
-        <h1>Home</h1>
+            <Archivo />
         </div>
     );
 }
