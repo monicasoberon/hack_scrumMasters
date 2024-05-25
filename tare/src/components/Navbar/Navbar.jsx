2b70da69c345953f52f60c88f22c64531d5ccf05
JulenHopp <julenhoppenstedt@gmail.com>
2024-05-25 02:41:44 -0400

    Sincronizando gits

--- tare/src/components/Navbar/Navbar.jsx
+++ tare/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,26 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Navbar.css';
 
 export default function Navbar({props}) {
   return (
-    <div>
-        <nav className="navbar">
-          <img src="" alt="foto de perfil" />
-          <div className="links">
-              <Link to="/">Home</Link>
-              <Link to="/Grupos">Grupos</Link>
-          </div>
-      </nav>
-    </div>
-
+    <nav className="sidebar">
+      <div className="sidebar-content">
+        <div className="profile-section">
+          {/* <img src={props.fotoPerfil} alt="foto de perfil" className="profile-image" />
+          <h3 className="profile-name">{props.nombre}</h3> */}
+          <img src="./src/assets/test/foto_perfil_prueba.svg" alt="foto de perfil" className="profile-image" />
+          <h3 className="profile-name">Julen Hoppenstedt Mandiola</h3>
+        </div>
+        <ul className="sidebar-links">
+          <li className="sidebar-link-item">
+            <NavLink to="/" className="sidebar-link">Home</NavLink>
+          </li>
+          <li className="sidebar-link-item">
+            <NavLink to="/Grupos" className="sidebar-link">Grupos</NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
