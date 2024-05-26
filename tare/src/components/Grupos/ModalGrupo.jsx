ce5321248d5960c422fb9f5745480b0c9f27c7cf
JulenHopp <julenhoppenstedt@gmail.com>
2024-05-26 12:05:49 -0600

    detalles esteticos

--- tare/src/components/Grupos/ModalGrupo.jsx
+++ tare/src/components/Grupos/ModalGrupo.jsx
@@ -4,13 +4,16 @@ import './ModalGrupo.css';
 const ModalGrupo = ({ isOpen, onClose, grupoStats, studentStats }) => {
     if (!isOpen) return null;
 
+    // Round the global average
+    const roundedPromedio = Number(grupoStats).toFixed(2);
+
     return (
         <div className="modal-overlay">
             <div className="modal-content">
                 <button onClick={onClose} className="modal-close-button">X</button>
-                <h2 className="modal-title">Group Information</h2>
-                <h3 className="modal-subtitle">Group Average: {grupoStats}</h3>
-                <h4 className="modal-section-title">Students</h4>
+                <h2 className="modal-title">Información del Grupo</h2>
+                <h3 className="modal-subtitle">Promedio global: {roundedPromedio}</h3>
+                <h4 className="modal-section-title">Estudiantes</h4>
                 <ul className="modal-student-list">
                     {studentStats.map((item, index) => (
                         <li key={index} className="modal-student-item">{item.estudiante}: {item.promedio}</li>
