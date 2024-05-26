875548df64e7d72db27755de7b8463618ac2d3bd
fermofou <fermofou@gmail.com>
2024-05-26 09:13:38 -0600

    add tara

--- tare/src/components/TutorTara/TutorTara.jsx
+++ tare/src/components/TutorTara/TutorTara.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './TutorTara.css'
+import 'backend/aiRoute.js'
 
 export default function TutorTara(props) {
     return (
 
         <div className="main-tutor">
             <div className='bubble-container'>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+                <p>Holaa! Mi nombre es Tara, puedes preguntarme lo que tu quieras. </p>
             </div>
             <img src="./src/assets/Tara.svg" alt="Tara" />
 
