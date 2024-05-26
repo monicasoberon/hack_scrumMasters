2e430589b0a69a30ccd7bb0099064355f5346045
A01722540 <a01722540@tec.mx>
2024-05-26 09:09:16 -0600

    fix de tara css

--- tare/src/components/TutorTara/TutorTara.jsx
+++ tare/src/components/TutorTara/TutorTara.jsx
@@ -8,8 +8,12 @@ export default function TutorTara(props) {
             <div className='bubble-container'>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
             </div>
+            <div className="sec-2">
             <img src="./src/assets/Tara.svg" alt="Tara" />
-
+            <div className='question'>
+                <p>Lorem ipsum dolor sit amet, </p>
+            </div>
+            </div>
             <form className='form-tara' action="POST">
                 <input type="text" />
                 <button type='submit'><img src="./src/assets/arrow-tara.svg" alt="^" /></button>
