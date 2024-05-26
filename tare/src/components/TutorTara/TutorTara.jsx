03f46d49198595c40bb6d0901faf593858d43c68
monicasoberon <a01254488@tec.mx>
2024-05-26 09:44:26 -0600

    commit

--- tare/src/components/TutorTara/TutorTara.jsx
+++ tare/src/components/TutorTara/TutorTara.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import './TutorTara.css'
-import 'backend/aiRoute.js'
 
 export default function TutorTara(props) {
     return (
 
         <div className="main-tutor">
             <div className='bubble-container'>
-                <p>Holaa! Mi nombre es Tara, puedes preguntarme lo que tu quieras. </p>
+                <p>Holaa! Mi nombre es Tara, te puedo ayudar a hacer planes diarios con un exámen, y a corregir examenes. Solo índica la fuente de información que deseas usar: libro SEP, archivo de notas, o, describeme el tema. Si quieres que corrija un exámen, ingresa el nombre de los archivos de examenes a corregir.</p>
             </div>
             <div className="sec-2">
             <img src="./src/assets/Tara.svg" alt="Tara" />
             <div className='question'>
-                <p>Lorem ipsum dolor sit amet, </p>
+                <p>Describeme lo que buscas aquí. </p>
             </div>
             </div>
             <form className='form-tara' action="POST">
